1d2a5c82996e3fda60371cbbc492bba044e86e8b
Alexandre <alex.lenedelec@gmail.com>
2019-09-22 14:33:29 +0200

    Ajout de la boucle de test avec un compteur de coups

--- main.cpp
+++ main.cpp
@@ -7,18 +7,27 @@ int main(){
 
 	int nombre=0;
 	int nombreMystere = 0;
-
+	int essais(0);
 	srand(time(NULL));
 	// genere un nombre aléatoire entre 0 et 99 et le stocke dans la variable nombreMystere
 	nombreMystere = (rand()%(100));
 
 	// afficher un texte à l'écran
-	cout << "entrez un nombre" << endl;
-	// lire le nombre tapé au clavier et l'enregistrer dans la variable nombre
-	cin >> nombre ;
+	cout << "entrez un nombre entre 1 et 100" << endl;
 
-	// affiche à l'écran le nombre saisi
-	cout << "vous avez saisi : " << nombre << endl;
+		while (nombreMystere!=nombre)
+		{
+		essais++;
+		// lire le nombre tapé au clavier et l'enregistrer dans la variable nombre
+		cin >> nombre ;
+			if(nombreMystere<nombre)
+				cout << "Trop Grand" << endl;
+			else if (nombreMystere>nombre)
+				cout << "Trop Petit" << endl;
+		}
+
+	// Affiche ce message quand le nombre est trouvé
+	cout << "Félicitation vous avez trouvé le nombre mystère (" << nombre << ") en : " << essais << " essais !" << endl;
 
 	//le programme se termine normalement en renvoyant 0
 	return 0;
