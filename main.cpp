b7c43bfd7054414650b90cec2242f245e9443797
Alexandre <alex.lenedelec@gmail.com>
2019-09-26 19:16:34 +0200

    Ajout du choix de la difficulté

--- main.cpp
+++ main.cpp
@@ -11,23 +11,67 @@ int main(){
 	int nombre=0;
 	int nombreMystere = 0;
 	int essais(0);
+	int mode=1;
+	int diff=0;
 	srand(time(NULL));
-	// genere un nombre aléatoire entre 0 et 99 et le stocke dans la variable nombreMystere
-	nombreMystere = (rand()%(100));
 
-	// afficher un texte à l'écran
-	cout << "Entrez un nombre entre 1 et 100 : ";
+	//afficher un texte à l'écran
+	cout << "mode un joueur tapez 1" << endl;
+	cout << "mode deux jouers tapez 2" << endl;
+	cin >> mode;
 
-		while (nombreMystere!=nombre)
+	//afficher un texte à l'écran
+	cout << "Niveau entre 1 et 100, tapez 1" << endl;
+	cout << "Niveau entre 1 et 1000, tapez 2" << endl;
+	cout << "Niveau entre 1 et 10000, tapez 3" << endl;
+	cin >> diff;
+		
+		//Choix de la diff
+		if (diff==1)
 		{
-		essais++;
-		// lire le nombre tapé au clavier et l'enregistrer dans la variable nombre
-		cin >> nombre ;
-			if(nombreMystere<nombre)
-				cout << "Trop Grand, essaye encore" << endl;
-			else if (nombreMystere>nombre)
-				cout << "Trop Petit, essaye encore" << endl;
+		nombreMystere=rand()%100 + 1;
+		diff=100;
 		}
+		else if (diff==2)
+		{
+		nombreMystere=rand()%1000 + 1;
+		diff=1000;
+		}
+		else
+		{
+		nombreMystere=rand()%10000 + 1;
+		diff=10000;
+		}
+
+		if (mode==2)
+		{
+		//afficher un texte à l'écran
+		cout << "Joueur 1 choisissez un nombre mystère entre 1 et " << diff << " :" << endl;
+
+		cout << nombreMystere << endl;
+
+		cin >> nombreMystere;
+		cout << "Joueur 2 entrer un nombre entre 1 et " << diff << " :" << endl;
+		}
+		else
+		{
+
+		// afficher un texte à l'écran
+		cout << "Entrez un nombre entre 1 et "<< diff << " :" << endl;
+		}
+		
+		cout << nombreMystere << endl;
+
+			while (nombreMystere!=nombre)
+			{
+			essais++;
+			// lire le nombre tapé au clavier et l'enregistrer dans la variable nombre
+			cin >> nombre ;
+				if(nombreMystere<nombre)
+					cout << "Trop Grand, essaye encore" << endl;
+				else if (nombreMystere>nombre)
+					cout << "Trop Petit, essaye encore" << endl;
+			}
 
 	// Affiche ce message quand le nombre est trouvé
 	cout << "Félicitation vous avez trouvé le nombre mystère (" << nombre << ") en : " << essais << " essais !" << endl;
